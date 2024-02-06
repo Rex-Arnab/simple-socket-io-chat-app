78fb88a15a844fd0c4a29e4d20e9121a6eefc276
Arnab Biswas <khanp4397@gmail.com>
2024-02-06 19:57:12 +0530

    added reply fix

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
+const { Server } = require("socket.io");
 const cors = require('cors');
 
 const app = express();
 app.use(cors());
-const server = http.createServer(app);
-const io = socketIo(server);
 
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
 
 // Handle connection
 io.on('connection', (socket) => {
-    console.log('A user connected');
+    const currentDate = new Date();
+    const formattedTime = currentDate.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true });
+
+    console.log('A user connected',
+        formattedTime
+    );
 
     // Handle disconnection
     socket.on('disconnect', () => {
         console.log('User disconnected');
     });
 
+    socket.on('reply', () => { console.log("a reply detected!") });
+
     // Handle chat message event
     socket.on('message', (msg) => {
+        console.log('Message received:', msg);
         if (msg === 'hi') {
             io.emit('message', 'Hello!');
         } else if (msg === 'bye') {
@@ -32,7 +45,6 @@ io.on('connection', (socket) => {
         console.log('Data received:', data);
         // Emit data back to the client
         io.emit('echo', data);
-
     });
 });
 
